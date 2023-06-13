4fce8656435da9df3585131335f6efd2244ca3be
marikadiblasio <marbla.albram@gmail.com>
2023-06-13 16:07:41 +0200

    add api projectcontroller ant routes index and show -

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index(){
+        $projects = Project::all();
+        return response()->json([
+            'success'=>true,
+            'results'=> $projects
+        ]);
+    }
+    public function show($slug){
+        $project = Project::where('slug', $slug)->first();
+        if($project){
+            return response()->json([
+            'success'=>true,
+            'results'=>$project
+        ]);
+        }else{
+            return response()->json([
+            'success'=>false,
+            'results'=>'Project not found'
+            ]);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,7 @@ Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')->group(
 // });
 
 require __DIR__.'/auth.php';
+
+Route::fallback(function () {
+    return redirect()->route('welcome');
+});
